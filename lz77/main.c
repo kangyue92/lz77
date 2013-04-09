bf50ef4c1d04bd4246678d07fa77ff25bbe826e5
kangyue <ky@kangyues-iMac.local>
2013-04-09 21:43:56 +0800

    Initial Commit

---
+++ lz77/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  lz77
+//
+//  Created by kangyue on 13-4-9.
+//  Copyright (c) 2013年 zhangyali. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
