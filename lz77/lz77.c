078a9bed7e1c2505e7f3d070728d968181e5c57b
ky1000ky2000 <ky1000ky2000@gmail.com>
2013-04-12 14:30:13 +0800

    Add comments in  lz77.c

--- lz77/lz77.c
+++ lz77/lz77.c
@@ -32,16 +32,16 @@ void CompressFile(FILE * inputFile, BITFILE * outputFile)
     
     while (look_ahead_bytes)
     {
-        if (match_length > look_ahead_bytes)
+        if (match_length > look_ahead_bytes)   //如果向后看一位匹配到的长度比原来的长，则设置最大长度为新的长度
             match_length = look_ahead_bytes;
         
-        if (match_length <= BREAK_EVEN)
+        if (match_length <= BREAK_EVEN)    //如果匹配到的长度比最小长度小,则单独输出此位置的字节,不进行编码
         {
             replace_count = 1;
             OutputBit(outputFile, 1);
             OutputBits(outputFile, (unsigned long)window[current_pos], 8);
         }
-        else
+        else    //否则输出编码后的标志位,距离,长度
         {
             replace_count = match_length;
             OutputBit(outputFile, 0);
@@ -49,7 +49,7 @@ void CompressFile(FILE * inputFile, BITFILE * outputFile)
             OutputBits(outputFile, (unsigned long)(match_length - BREAK_EVEN - 1),LENGTH_BIT_COUNT);
         }
         
-        for (i = 0; i < replace_count; i++)
+        for (i = 0; i < replace_count; i++)   //移动当前位置指针到尚未处理的位置上
         {
             c = getc(inputFile);
             if (EOF == c)
@@ -88,7 +88,7 @@ void CompressFile(FILE * inputFile, BITFILE * outputFile)
             }
         }
     }
-    OutputBit(outputFile, 0);
+    OutputBit(outputFile, 0);    //输出结束标记
     OutputBits(outputFile, (unsigned long)END_OF_STREAM, INDEX_BIT_COUNT);
 }
 
@@ -106,14 +106,14 @@ void ExpandFile(BITFILE * inputFile, FILE * outputFile)
     current_pos = 1;
     while (1)
     {
-        if (InputBit(inputFile))
+        if (InputBit(inputFile))    //如果读到1,则输出后面8位为一个字节
         {
             c = (int)InputBits(inputFile, 8);
             putc(c, outputFile);
             window[current_pos] = (unsigned char)c;
             current_pos = MOD_WINDOW(current_pos + 1);
         }
-        else
+        else    //如果读到0,则按照,位置,长度,找到前面的相同部分,并输出
         {
             match_pos = (int)InputBits(inputFile, INDEX_BIT_COUNT);
             if (END_OF_STREAM == match_pos)
